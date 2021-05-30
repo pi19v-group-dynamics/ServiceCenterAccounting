a1182399f7e1c667b495d1c63d52f02211b5a59c
Acer <Acer@DESKTOP-75AVNMD>
2021-05-30 20:17:40 +0300

    Создание интерфейса формы общего отчёта предприятия

--- ServiceCenterAccounting/Reports_Creator.cs
+++ ServiceCenterAccounting/Reports_Creator.cs
@@ -16,5 +16,10 @@ namespace ServiceCenterAccounting
         {
             InitializeComponent();
         }
+
+        private void comboBox1_SelectedIndexChanged(object sender, EventArgs e)
+        {
+
+        }
     }
 }
