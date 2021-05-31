6191095f666d159e0af614e8876610b1a1fbe099
Kovalenko_Alexander <ilondontami@gmail.com>
2021-05-31 12:27:08 +0300

    Реализация добавления заказа (промежуточное)

--- ServiceCenterAccounting/AddingAndChangingOrders.Designer.cs
+++ ServiceCenterAccounting/AddingAndChangingOrders.Designer.cs
@@ -29,19 +29,195 @@ namespace ServiceCenterAccounting
         /// </summary>
         private void InitializeComponent()
         {
+            this.clientField = new System.Windows.Forms.TextBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.workersList = new System.Windows.Forms.ComboBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.comboBox2 = new System.Windows.Forms.ComboBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.button2 = new System.Windows.Forms.Button();
+            this.servicesList = new System.Windows.Forms.ComboBox();
+            this.label7 = new System.Windows.Forms.Label();
+            this.addBut = new System.Windows.Forms.Button();
+            this.cancleBut = new System.Windows.Forms.Button();
+            this.dateTimePicker = new System.Windows.Forms.DateTimePicker();
+            this.dateLable = new System.Windows.Forms.Label();
             this.SuspendLayout();
+            // 
+            // clientField
+            // 
+            this.clientField.Location = new System.Drawing.Point(87, 52);
+            this.clientField.Name = "clientField";
+            this.clientField.ReadOnly = true;
+            this.clientField.Size = new System.Drawing.Size(161, 22);
+            this.clientField.TabIndex = 0;
+            // 
+            // button1
+            // 
+            this.button1.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.button1.Location = new System.Drawing.Point(254, 46);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(34, 33);
+            this.button1.TabIndex = 1;
+            this.button1.Text = "+";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // workersList
+            // 
+            this.workersList.FormattingEnabled = true;
+            this.workersList.Location = new System.Drawing.Point(351, 52);
+            this.workersList.Name = "workersList";
+            this.workersList.Size = new System.Drawing.Size(163, 24);
+            this.workersList.TabIndex = 2;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label1.Location = new System.Drawing.Point(84, 23);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(75, 20);
+            this.label1.TabIndex = 3;
+            this.label1.Text = "Клиент:";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label2.Location = new System.Drawing.Point(347, 23);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(94, 20);
+            this.label2.TabIndex = 4;
+            this.label2.Text = "Работник:";
+            // 
+            // comboBox2
+            // 
+            this.comboBox2.FormattingEnabled = true;
+            this.comboBox2.Location = new System.Drawing.Point(85, 143);
+            this.comboBox2.Name = "comboBox2";
+            this.comboBox2.Size = new System.Drawing.Size(163, 24);
+            this.comboBox2.TabIndex = 5;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label3.Location = new System.Drawing.Point(82, 111);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(145, 20);
+            this.label3.TabIndex = 6;
+            this.label3.Text = "Тип устройства:";
+            // 
+            // button2
+            // 
+            this.button2.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.button2.Location = new System.Drawing.Point(254, 137);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(34, 33);
+            this.button2.TabIndex = 7;
+            this.button2.Text = "+";
+            this.button2.UseVisualStyleBackColor = true;
+            // 
+            // servicesList
+            // 
+            this.servicesList.FormattingEnabled = true;
+            this.servicesList.Location = new System.Drawing.Point(351, 146);
+            this.servicesList.Name = "servicesList";
+            this.servicesList.Size = new System.Drawing.Size(163, 24);
+            this.servicesList.TabIndex = 15;
+            // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label7.Location = new System.Drawing.Point(347, 111);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(99, 20);
+            this.label7.TabIndex = 16;
+            this.label7.Text = "Тип работ:";
+            // 
+            // addBut
+            // 
+            this.addBut.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.addBut.Location = new System.Drawing.Point(496, 323);
+            this.addBut.Name = "addBut";
+            this.addBut.Size = new System.Drawing.Size(110, 34);
+            this.addBut.TabIndex = 27;
+            this.addBut.Text = "Сохранить";
+            this.addBut.UseVisualStyleBackColor = true;
+            // 
+            // cancleBut
+            // 
+            this.cancleBut.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.cancleBut.Location = new System.Drawing.Point(12, 323);
+            this.cancleBut.Name = "cancleBut";
+            this.cancleBut.Size = new System.Drawing.Size(110, 34);
+            this.cancleBut.TabIndex = 26;
+            this.cancleBut.Text = "Отмена";
+            this.cancleBut.UseVisualStyleBackColor = true;
+            // 
+            // dateTimePicker
+            // 
+            this.dateTimePicker.Format = System.Windows.Forms.DateTimePickerFormat.Short;
+            this.dateTimePicker.Location = new System.Drawing.Point(254, 224);
+            this.dateTimePicker.Name = "dateTimePicker";
+            this.dateTimePicker.Size = new System.Drawing.Size(122, 22);
+            this.dateTimePicker.TabIndex = 10;
+            // 
+            // dateLable
+            // 
+            this.dateLable.AutoSize = true;
+            this.dateLable.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.dateLable.Location = new System.Drawing.Point(250, 201);
+            this.dateLable.Name = "dateLable";
+            this.dateLable.Size = new System.Drawing.Size(142, 20);
+            this.dateLable.TabIndex = 13;
+            this.dateLable.Text = "Дата принятия:";
             // 
             // AddingAndChangingOrders
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(1097, 445);
+            this.ClientSize = new System.Drawing.Size(618, 366);
+            this.Controls.Add(this.addBut);
+            this.Controls.Add(this.cancleBut);
+            this.Controls.Add(this.label7);
+            this.Controls.Add(this.servicesList);
+            this.Controls.Add(this.dateLable);
+            this.Controls.Add(this.dateTimePicker);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.comboBox2);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.workersList);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.clientField);
             this.Name = "AddingAndChangingOrders";
             this.Text = "AddingAndChangingOrders";
+            this.Load += new System.EventHandler(this.AddingAndChangingOrders_Load);
             this.ResumeLayout(false);
+            this.PerformLayout();
 
         }
 
         #endregion
+
+        private System.Windows.Forms.TextBox clientField;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.ComboBox workersList;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.ComboBox comboBox2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.ComboBox servicesList;
+        private System.Windows.Forms.Label label7;
+        private System.Windows.Forms.Button addBut;
+        private System.Windows.Forms.Button cancleBut;
+        private System.Windows.Forms.DateTimePicker dateTimePicker;
+        private System.Windows.Forms.Label dateLable;
     }
 }
---
+++ ServiceCenterAccounting/AddingDevice.cs
@@ -0,0 +1,301 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace ServiceCenterAccounting
+{
+
+    public enum Devices
+    {
+        Computer,
+        Phone,
+        Laptop,
+        Other
+    }
+
+    public partial class AddingDevice : Form
+    {
+        Device device;
+        Devices type;
+
+        public AddingDevice(Devices type)
+        {
+            InitializeComponent();
+            this.type = type;
+            Filling();
+        }
+
+        private void Filling()
+        {
+            Warning0.Visible = false;
+            Warning1.Visible = false;
+            Warning2.Visible = false;
+            Warning3.Visible = false;
+            Warning4.Visible = false;
+            Warning5.Visible = false;
+            Warning6.Visible = false;
+            Warning7.Visible = false;
+            Warning8.Visible = false;
+            Warning9.Visible = false;
+
+            label1.Visible = true;
+            label1.Text = "Производитель:";
+            textBox1.Visible = true;
+
+            if (type == Devices.Computer)
+            {
+                label1.Visible = true;
+                label1.Text = "Модель:";
+                textBox2.Visible = true;
+                label2.Visible = true;
+                label2.Text = "Процессор:";
+                textBox3.Visible = true;
+                label3.Visible = true;
+                label3.Text = "Видеокарта:";
+                textBox4.Visible = true;
+                label4.Visible = true;
+                label4.Text = "Блок питания:";
+                textBox5.Visible = true;
+                label5.Visible = true;
+                label5.Text = "Охлаждение:";
+                textBox6.Visible = true;
+                label6.Visible = true;
+                label6.Text = "ОЗУ:";
+                numericUpDown1.Visible = true;
+                labelNum1.Visible = true;
+                labelNum1.Text = "Количество накопителей:";
+                numericUpDown2.Visible = true;
+                labelNum2.Visible = true;
+                labelNum2.Text = "Суммарный объм накопителей:";
+                numericUpDown3.Visible = true;
+                labelNum3.Visible = true;
+                labelNum3.Text = "Объём ОЗУ:";
+                text.Visible = true;
+            }
+            else if(type == Devices.Phone)
+            {
+                textBox3.Visible = true;
+                label3.Visible = true;
+                label3.Text = "Модель:";
+                textBox5.Visible = true;
+                label5.Visible = true;
+                label5.Text = "IMEI:";
+            }
+            else if(type == Devices.Laptop)
+            {
+                textBox2.Visible = true;
+                label2.Visible = true;
+                label2.Text = "Модель:";
+                textBox3.Visible = true;
+                label3.Visible = true;
+                label3.Text = "Процессор:";
+                textBox5.Visible = true;
+                label5.Visible = true;
+                label5.Text = "ОЗУ:";
+                numericUpDown1.Visible = true;
+                labelNum2.Visible = true;
+                labelNum2.Text = "Объём ОЗУ:";
+                numericUpDown2.Visible = true;
+                labelNum2.Visible = true;
+                labelNum2.Text = "Количество накопителей:";
+                numericUpDown3.Visible = true;
+                labelNum3.Visible = true;
+                labelNum3.Text = "Суммарный объём памяти накопителей:";
+            }
+            else
+            {
+                textBox2.Visible = true;
+                label2.Visible = true;
+                label2.Text = "Модель:";
+                comboBox1.Visible = true;
+                label3.Visible = true;
+                label3.Text = "Тип компонента:";
+
+                comboBox1.DataSource = Connect.Select("select id_component_or_other_device_type as id, " +
+                    "name_component_or_other_device_type as name " +
+                    "from component_or_other_device_types");
+                comboBox1.DisplayMember = "name";
+                comboBox1.ValueMember = "id";
+            }
+        }
+
+        private void AddingDevice_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void addBut_Click(object sender, EventArgs e)
+        {
+            if (label1.Visible == true && label1.Text == "")
+            {
+                Warning1.Visible = true;
+                MessageBox.Show("Все поля должны быть заполнены!", "Предупреждение", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+            else if (label2.Visible == true && label2.Text == "")
+            {
+                Warning2.Visible = true;
+                MessageBox.Show("Все поля должны быть заполнены!", "Предупреждение", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+            else if (label3.Visible == true && label3.Text == "")
+            {
+                Warning3.Visible = true;
+                MessageBox.Show("Все поля должны быть заполнены!", "Предупреждение", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+            else if (label4.Visible == true && label4.Text == "")
+            {
+                Warning4.Visible = true;
+                MessageBox.Show("Все поля должны быть заполнены!", "Предупреждение", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+            else if (label5.Visible == true && label5.Text == "")
+            {
+                Warning5.Visible = true;
+                MessageBox.Show("Все поля должны быть заполнены!", "Предупреждение", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+            else if (label6.Visible == true && label6.Text == "")
+            {
+                Warning6.Visible = true;
+                MessageBox.Show("Все поля должны быть заполнены!", "Предупреждение", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+            else if (labelNum1.Visible == true && numericUpDown1.Text == "")
+            {
+                Warning7.Visible = true;
+                MessageBox.Show("Все поля должны быть заполнены!", "Предупреждение", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+            else if (labelNum2.Visible == true && numericUpDown2.Text == "")
+            {
+                Warning8.Visible = true;
+                MessageBox.Show("Все поля должны быть заполнены!", "Предупреждение", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+            else if (labelNum3.Visible == true && numericUpDown3.Text == "")
+            {
+                Warning9.Visible = true;
+                MessageBox.Show("Все поля должны быть заполнены!", "Предупреждение", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+            else if (text.Visible == true && text.Text == "")
+            {
+                Warning0.Visible = true;
+                MessageBox.Show("Все поля должны быть заполнены!", "Предупреждение", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+            else 
+            {
+                if (type == Devices.Computer)
+                {
+                    device = new Device();
+                    device.Text1 = label1.Text;
+                    device.Text2 = label2.Text;
+                    device.Text3 = label3.Text;
+                    device.Text4 = label4.Text;
+                    device.Text5 = label5.Text;
+                    device.Text6 = label6.Text;
+                    device.Num1 = (int)numericUpDown1.Value;
+                    device.Num2 = (int)numericUpDown2.Value;
+                    device.Num3 = (int)numericUpDown3.Value;
+                    device.MultiText = text.Text;
+                }
+                else if (type == Devices.Phone)
+                {
+                    device.Text1 = textBox1.Text;
+                    device.Text2 = textBox3.Text;
+                    device.Text3 = textBox5.Text;
+                }
+                else if (type == Devices.Laptop) 
+                {
+                    device.Text1 = textBox1.Text;
+                    device.Text2 = textBox2.Text;
+                    device.Text3 = textBox3.Text;
+                    device.Text4 = textBox5.Text;
+                    device.Num1 = (int)numericUpDown1.Value;
+                    device.Num2 = (int)numericUpDown2.Value;
+                    device.Num3 = (int)numericUpDown3.Value;
+                }
+                else
+                {
+                    device.Text1 = textBox1.Text;
+                    device.Text2 = textBox2.Text;
+                    device.Num1 =  (int)comboBox1.SelectedValue;
+                }
+            }
+        }
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+            Warning1.Visible = false;
+        }
+
+        private void textBox2_TextChanged(object sender, EventArgs e)
+        {
+            Warning2.Visible = false;
+        }
+
+        private void comboBox1_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            Warning3.Visible = false;
+        }
+
+        private void textBox3_TextChanged(object sender, EventArgs e)
+        {
+            Warning3.Visible = false;
+        }
+
+        private void textBox4_TextChanged(object sender, EventArgs e)
+        {
+            Warning4.Visible = false;
+        }
+
+        private void textBox5_TextChanged(object sender, EventArgs e)
+        {
+            Warning5.Visible = false;
+        }
+
+        private void textBox6_TextChanged(object sender, EventArgs e)
+        {
+            Warning6.Visible = false;
+        }
+
+        private void numericUpDown1_ValueChanged(object sender, EventArgs e)
+        {
+            Warning7.Visible = false;
+        }
+
+        private void numericUpDown2_ValueChanged(object sender, EventArgs e)
+        {
+            Warning8.Visible = false;
+        }
+
+        private void numericUpDown3_ValueChanged(object sender, EventArgs e)
+        {
+            Warning9.Visible = false;
+        }
+
+        private void text_TextChanged(object sender, EventArgs e)
+        {
+            Warning0.Visible = false;
+        }
+
+        private void cancleBut_Click(object sender, EventArgs e)
+        {
+            Close();
+        }
+    }
+
+    public class Device
+    {
+        public string Text1 { get; set; }
+        public string Text2 { get; set; }
+        public string Text3 { get; set; }
+        public string Text4 { get; set; }
+        public string Text5 { get; set; }
+        public string Text6 { get; set; }
+        public int Num1 { get; set; }
+        public int Num2 { get; set; }
+        public int Num3 { get; set; }
+        public string MultiText { get; set; }
+    }
+
+}
